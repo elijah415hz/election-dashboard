be5e7a664839905642b51c4ddb8793cd5a6c798c
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-09-22 11:05:40 -0700

    Initial parsing

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 
 // Ajax request to Google Civic Info
-var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyAY3D8Rvr86w2k066vbIV1mpziRwWCO2kc&address=3121 franklin ave e seattle wa"
+var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyAY3D8Rvr86w2k066vbIV1mpziRwWCO2kc&address=267 w 70th st new york new york"
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
     console.log(response);
+    var offices = response.offices
+    var officials = response.officials
+    var federal = []
+    var state = []
+    var local = []
+    for (let i=0; i<offices.length; i++) {
+        var level = offices[i].levels[0]
+        console.log(level);
+        switch (level) { 
+        case "country":
+            federal.push(officials[i])
+            break;
+        case "administrativeArea1":
+            state.push(officials[i])
+            break;
+        case "administrativeArea2":
+        case "locality":
+            local.push(officials[i])
+            break;
+        }
+    }
 })
